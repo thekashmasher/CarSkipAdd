bdae32f38bfa2e6fac70eaa0f24ee539661481a2
thekashmasher <120293801+thekashmasher@users.noreply.github.com>
2024-11-08 15:12:20 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,16 +1,12 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
-    #1;
   end
 
   // Wire up the inputs and outputs:
@@ -22,28 +18,31 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #10 clk = ~clk; // 100 MHz clock
+  end
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
+  // Declare power supply signals
+  supply1 VPWR;
+  supply0 VGND;
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+  // Instantiate the design under test (DUT)
+  tt_um_carryskip_adder8 user_project (
+  `ifdef GL_TEST
+        .VPWR(VPWR),
+        .VGND(VGND),
+  `endif
+        .ui_in  (ui_in),    // Dedicated inputs
+        .uo_out (uo_out),   // Dedicated outputs
+        .uio_in (uio_in),   // IOs: Input path
+        .uio_out(uio_out),  // IOs: Output path
+        .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+        .ena    (ena),      // enable - goes high when design is selected
+        .clk    (clk),      // clock
+        .rst_n  (rst_n)     // active low reset
+    );
 
 endmodule
