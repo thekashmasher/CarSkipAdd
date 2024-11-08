035be10137f4bf67b11c6a9c62df59776c17bbc2
thekashmasher <120293801+thekashmasher@users.noreply.github.com>
2024-11-08 15:02:38 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,8 +6,8 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] a,    // Dedicated inputs
+    output wire [7:0] b,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
